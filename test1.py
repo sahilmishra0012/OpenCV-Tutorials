084b8abff784000ed84772f819a98c5ceb0c3d5e
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-16 23:38:56 +0530

    New Changes

--- test1.py
+++ test1.py
@@ -17,8 +17,8 @@ cv2.imshow('Image Window',img)
 # remains opened.
 k=cv2.waitKey(0)
 
-if k==27:
+if k==27:#  27 is for escape character
     cv2.destroyAllWindows()# To destroy all the windows
-# Write Image
+# Write Image when s is pressed.
 elif k==ord('s'):
     cv2.imwrite('bird.jpg',img)
