d2e0d8d921ccf75a07859ab70b85543d16dd220b
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-16 23:32:38 +0530

    New Changes

--- test1.py
+++ test1.py
@@ -3,6 +3,16 @@
 import cv2
 
 # Read Images
-# imread(filename,flag)  flag=1=>Color Image flag=0=>Grayscale flag=-1=>Original Image
+# img=cv2.imread(filename,flag)  flag=1=>Color Image flag=0=>Grayscale flag=-1=>Original Image
 img=cv2.imread('robin.jpg',1)
 print(img.shape)
+
+
+# Show Image
+# cv2.imshow('Window Name',img)
+
+cv2.imshow('Image Window',img)
+
+#It prevents the window from closing immediately be giving it delay. Put 0 to close it by user
+cv2.waitKey(0)
+cv2.destroyAllWindows()# To destroy all the windows
