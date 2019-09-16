f2b7ca03f518d90f8b10a130fe9031be3d348cd5
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-16 23:26:19 +0530

    New Chnages

--- test1.py
+++ test1.py
@@ -1,1 +1,8 @@
+# Program to read, write and show images
+
 import cv2
+
+# Read Images
+# imread(filename,flag)  flag=1=>Color Image flag=0=>Grayscale flag=-1=>Original Image
+img=cv2.imread('robin.jpg',1)
+print(img.shape)
