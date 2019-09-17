35a415b9959be8e67ae484dd29d91015fa03ce0b
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-17 09:17:16 +0530

    New Changes

--- video.py
+++ video.py
@@ -1,17 +1,17 @@
+# Video capture using camera of android phone.
+# Using android app IP Webcam to connect webcam to localhost.
 import cv2
-
-
-#Provide filename to read images or 0 to access camera
-cap=cv2.VideoCapture(1)
+import numpy as np
+import requests
 
 while(True):
-    ret,frame=cap.read()
+#Provide filename to read images or 0 to access camera
+    img_res = requests.get("http://192.168.43.18:8080/shot.jpg")# Private IP
+    img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
+    cap=cv2.imdecode(img_arr,-1)
+
+    cv2.imshow('frame', cap)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
-cap.release()
 cv2.destroyAllWindows()
-
-
-# Currently camera drivers are missing on my system.
-# So, output file will be added only after installing drivers.
