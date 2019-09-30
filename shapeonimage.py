c5493bf5a336ea535cb2f1b60220e74b7386270b
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-30 23:35:20 +0530

    New Changes

--- shapeonimage.py
+++ shapeonimage.py
@@ -3,11 +3,14 @@
 import cv2
 
 img=cv2.imread('robin.jpg',1)
-print(img.shape)
+
+#cv2.line(img,(Start Point Tuple),(End Point Tuple),(RGB Tuple),thickness)
+img=cv2.line(img,(100,100),(400,500),(255,0,0),5)
+
 cv2.imshow('Image Window',img)
 k=cv2.waitKey(0)
 
-if k==27:#  27 is for escape character
+if k==ord('q'):#  27 is for escape character
     cv2.destroyAllWindows()# To destroy all the windows
 # Write Image when s is pressed.
 elif k==ord('s'):
