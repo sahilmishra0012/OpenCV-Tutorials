9752d8a5db773f2967a882f834b7439d2bcccd69
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-30 23:14:11 +0530

    New Changes

--- video.py
+++ video.py
@@ -8,6 +8,8 @@ while(True):
 #Provide filename to read images or 0 to access camera
     img_res = requests.get("http://192.168.43.18:8080/shot.jpg")# Private IP
     img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
+
+    cv2.cvtColor(img_arr,cv2.COLOR_BGR2GRAY)
     cap=cv2.imdecode(img_arr,-1)
 
     cv2.imshow('frame', cap)
