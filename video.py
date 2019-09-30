c0ceeec043f746202ad2fd0a85dcd0e7b2b02d5a
Sahil Mishra <sahilmishra0012@outlook.com>
2019-09-30 23:29:09 +0530

    New Changes

--- video.py
+++ video.py
@@ -4,16 +4,21 @@ import cv2
 import numpy as np
 import requests
 
+
 while(True):
 #Provide filename to read images or 0 to access camera
-    img_res = requests.get("http://192.168.43.18:8080/shot.jpg")# Private IP
+    img_res = requests.get("http://192.168.43.79:8080/shot.jpg")# Private IP
     img_arr = np.array(bytearray(img_res.content), dtype = np.uint8)
 
-    cv2.cvtColor(img_arr,cv2.COLOR_BGR2GRAY)
+    
     cap=cv2.imdecode(img_arr,-1)
 
-    cv2.imshow('frame', cap)
+    gray=cv2.cvtColor(cap,cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
 
     if cv2.waitKey(1) & 0xFF==ord('q'):
         break
+
+
 cv2.destroyAllWindows()
