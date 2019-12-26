69947bde306b26a3b028d2e5f27e55dbdb7aab32
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-26 23:23:34 +0530

    New Changes

--- 5.mouseevent.py
+++ 5.mouseevent.py
@@ -1,7 +1,26 @@
 import numpy as np
 import cv2
 
-events=[i for i in dir(cv2) if 'EVENT' in i]
 
-print(len(events))
-print(events)
+
+def click_event(event,x,y,flags,param):
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print(x," ",y)
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        strXY=str(x)+" "+str(y)
+        cv2.putText(img,strXY,(x,y),font,0.25,(255,255,0),1)
+        cv2.imshow('image',img)
+    if event==cv2.EVENT_RBUTTONDOWN:
+        red=img[y,x,0]
+        green=img[y,x,1]
+        blue=img[y,x,2]
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        strXY=str(red)+" "+str(green)+" "+str(blue)
+        cv2.putText(img,strXY,(x,y),font,0.25,(255,255,0),1)
+        cv2.imshow('image',img) 
+
+img=cv2.imread('bird.jpg')
+cv2.imshow('image',img)
+cv2.setMouseCallback('image',click_event)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
