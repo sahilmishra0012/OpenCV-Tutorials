5f468e8a66f016c8f08c792f9f61d69f3d02fa8c
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-28 12:11:30 +0530

    New Changes

--- 8.rotation.py
+++ 8.rotation.py
@@ -3,8 +3,8 @@ import numpy as np
 img=cv2.imread('robin.jpg',1)
 height,width=img.shape[:2]
 quarter_h,quarter_w=height/4,width/4
-T=np.float32([[1,0,quarter_w],[0,1,quarter_h]])
-translated_img=cv2.warpAffine(img,T,(height,width))
+r_mat=cv2.getRotationMatrix2D((width/2,height/2),90,1)
+translated_img=cv2.warpAffine(img,r_mat,(height,width))
 cv2.imshow('Image Window',translated_img)
 
 k=cv2.waitKey(100000)
