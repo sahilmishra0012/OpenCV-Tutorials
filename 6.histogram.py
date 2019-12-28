46345059f95a334b3a41c308ad490dc0415e4bf6
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-28 11:10:52 +0530

    New Changes

--- 6.histogram.py
+++ 6.histogram.py
@@ -3,12 +3,9 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img=cv2.imread('robin.jpg',1)
-
 color=('b','g','r')
-
 for i,col in enumerate(color):
     histogram=cv2.calcHist([img],[i],None,[256],[0,256])
     plt.plot(histogram,color=col)
     plt.xlim([0,256])
-
 plt.show()
