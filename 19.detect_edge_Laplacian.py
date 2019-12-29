0785769add81d9a0cce96bf815242bff56f19e32
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-29 09:34:36 +0530

    New Changes

---
+++ 19.detect_edge_Laplacian.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+img=cv2.imread('bird.jpg',1)
+height,width=img.shape[:2]
+start_row,start_col=int(height*0.2),int(width*0.2)
+end_row,end_col=int(height*0.6),int(width*0.6)
+img=img[start_row:end_row,start_col:end_col]
+
+sobel_or=cv2.Laplacian(img,cv2.CV_64F)
+
+cv2.imshow('Image Window',sobel_or)
+k=cv2.waitKey(100000)
