5af46e8affd6c6c174131663d9d30a39a6480a18
Sahil Mishra <sahilmishra0012@outlook.com>
2019-12-30 10:02:25 +0530

    New Changes

---
+++ 21.sorting_contours.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+img=cv2.imread('bird.jpg',1)
+height,width=img.shape[:2]
+start_row,start_col=int(height*0.2),int(width*0.2)
+end_row,end_col=int(height*0.6),int(width*0.6)
+img=img[start_row:end_row,start_col:end_col]
+
+gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+
+edges=cv2.Canny(gray,30,200)
+contours,heirarchy=cv2.findContours(edges,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+sorted_contours=sorted(contours,key=cv2.contourArea,reverse=True)
+sorted_contours=sorted_contours[0:2]
+cv2.drawContours(img,sorted_contours,-1,(0,255,0),3)
+cv2.imshow('Image Window',img)
+k=cv2.waitKey(100000)
